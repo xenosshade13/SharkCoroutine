3910456a6421d6ee95b553dbac70b0a5c6bfa77c
xenosshade13 <tbrodows@stumail.jccc.edu>
2021-03-21 04:02:26 -0500

    finalized
    this is the official project

---
+++ Shark/Assets/scripts/bulletMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletMovement : MonoBehaviour
+{
+    public float lifeTime = 2f;
+    public float speed = 12f;
+
+    private float lifeTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimer = lifeTime;
+        Physics.GetIgnoreLayerCollision(10, 9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Shark/Assets/scripts/fireControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireControl : MonoBehaviour
+{
+    public float clip = 5;
+    public GameObject bulletPrefab;
+    public Transform emmiter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            clip = 5;
+            Debug.Log("Reloaded");
+        }
+
+        if (clip == 0)
+        {
+            return;
+        }
+      
+        if (Input.GetKeyDown(KeyCode.Space) && clip > 0)
+        {
+            GameObject newbullet = GameObject.Instantiate(bulletPrefab);
+            newbullet.transform.position = emmiter.position;
+            newbullet.transform.forward = this.transform.forward;
+            clip --;
+            Debug.Log("Chomp!");
+        }
+    }
+}
